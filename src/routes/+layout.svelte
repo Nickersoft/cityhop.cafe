35b08f7076469de27fa90dade4261353879c1172
Tyler Nickerson <nickersoft@gmail.com>
2023-05-09 22:13:13 -0700

    wip

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,32 @@
 <script>
+	import { onMount } from 'svelte';
+	import { pwaInfo } from 'virtual:pwa-info';
+
 	import 'iconify-icon';
 
 	import '../app.postcss';
 
-	import { onMount } from 'svelte';
+	onMount(async () => {
+		if (pwaInfo) {
+			const { registerSW } = await import('virtual:pwa-register');
+			registerSW({
+				immediate: true,
+				onRegistered(r) {
+					r &&
+						setInterval(() => {
+							console.log('Checking for sw update');
+							r.update();
+						}, 20000);
+					console.log(`SW Registered: ${r}`);
+				},
+				onRegisterError(error) {
+					console.log('SW registration error', error);
+				}
+			});
+		}
+	});
+
+	$: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : '';
 
 	onMount(() => {
 		navigator.mediaSession.setActionHandler('play', function () {
@@ -28,6 +51,7 @@
 </script>
 
 <svelte:head>
+	{@html webManifest}
 	<title>CityHop | Aesthetic walks and drives from around the world</title>
 </svelte:head>
 
