968c5c645fb621388152372ef20eb165cc6a9195
Tyler Nickerson <nickersoft@gmail.com>
2023-05-25 20:52:36 -0700

    feat(*): update PostHog Node settings

--- src/routes/api/heartbeat/+server.ts
+++ src/routes/api/heartbeat/+server.ts
@@ -4,12 +4,18 @@ import type { RequestHandler } from './$types';
 
 import { PUBLIC_PH_TOKEN } from '$env/static/public';
 
-const ph = new PostHog(PUBLIC_PH_TOKEN);
-
 export const POST: RequestHandler = async ({ request }) => {
+	const ph = new PostHog(PUBLIC_PH_TOKEN, {
+		host: 'https://app.posthog.com',
+		flushAt: 1,
+		flushInterval: 0
+	});
+
 	const body = await request.json();
 
 	ph.capture({ distinctId: body.distinctID, event: 'heartbeat' });
+
+	await ph.shutdownAsync();
 
 	return new Response(JSON.stringify({ success: true }));
 };
