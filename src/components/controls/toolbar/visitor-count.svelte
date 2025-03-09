740f946985e9ff16a86b11465d6a0a4c43b453c1
Tyler Nickerson <nickersoft@gmail.com>
2025-03-08 21:45:15 -0500

    fix toolbar

--- src/components/controls/toolbar/visitor-count.svelte
+++ src/components/controls/toolbar/visitor-count.svelte
@@ -23,12 +23,12 @@
 	});
 </script>
 
-{#if visitorCount && visitorCount > 0}
-	<div transition:fade={{ duration: 500 }} class="flex flex-row items-center gap-0">
+<div transition:fade={{ duration: 500 }} class="flex flex-row items-center gap-0">
+	{#if visitorCount && visitorCount > 0}
 		<HandPeace class="size-4" />
 		<Typography variant="body" color="subtle" class="flex-1 pr-4 pl-2">
 			<span class="text-foreground">{visitorCount}</span> other
 			{visitorCount === 1 ? 'person' : 'people'} are vibin here
 		</Typography>
-	</div>
-{/if}
+	{/if}
+</div>
