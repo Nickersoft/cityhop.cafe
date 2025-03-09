fa9f29458867c73b6381d02501f7f53b5159d17d
Tyler Nickerson <nickersoft@gmail.com>
2025-03-08 21:43:35 -0500

    fixes

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,14 +12,14 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
-    alias: {
-      $actions: './src/actions',
-      $components: './src/components',
-      $consts: './src/consts',
-      $lib: './src/lib',
-      $styles: './src/styles',
-      $server: './src/server',
-    }
+		alias: {
+			$actions: './src/actions',
+			$components: './src/components',
+			$consts: './src/consts',
+			$lib: './src/lib',
+			$styles: './src/styles',
+			$server: './src/server'
+		}
 	}
 };
 
