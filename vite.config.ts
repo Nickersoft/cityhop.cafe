dc9175aacf5eafcc2017a5a7eb34b7485595c175
Tyler Nickerson <nickersoft@gmail.com>
2026-04-08 18:40:41 -0400

    fix(ci): fix CI

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
-/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 
-import { svelteTesting } from '@testing-library/svelte/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 import icons from 'unplugin-icons/vite';
@@ -14,31 +12,5 @@ export default defineConfig({
 		icons({
 			compiler: 'svelte'
 		})
-	],
-
-	test: {
-		workspace: [
-			{
-				extends: './vite.config.ts',
-				plugins: [svelteTesting()],
-				test: {
-					name: 'client',
-					environment: 'jsdom',
-					clearMocks: true,
-					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-					exclude: ['src/lib/server/**'],
-					setupFiles: ['./vitest-setup-client.ts']
-				}
-			},
-			{
-				extends: './vite.config.ts',
-				test: {
-					name: 'server',
-					environment: 'node',
-					include: ['src/**/*.{test,spec}.{js,ts}'],
-					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
-				}
-			}
-		]
-	}
+	]
 });
